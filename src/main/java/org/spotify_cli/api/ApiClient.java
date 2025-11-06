82105e6bbb123ac379b87c255d07aa768245383b
a22ricardoadc <a22ricardoadc@iesteis.es>
2025-11-06 13:38:51 +0100

    paquetes creados y api client TODO

---
+++ src/main/java/org/spotify_cli/api/ApiClient.java
@@ -0,0 +1,12 @@
+package org.spotify_cli.api;
+
+public class ApiClient {
+
+    /**
+     * Esta funcion debe ejecutarse cada vez que se instancia la aplicacion
+     */
+    // TODO public String getTokenAPI() {}
+    // TODO intentar hacer que justo calcule la ultima hora para aprovechar mejor los tokens.
+
+
+}
