0394a73ae11f1821566195904c0b332a9f283ffb
a22ricardoadc <a22ricardoadc@iesteis.es>
2025-11-06 13:40:06 +0100

    clases de storage descritas

---
+++ src/main/java/org/spotify_cli/storage/CsvExporter.java
@@ -0,0 +1,7 @@
+package org.spotify_cli.storage;
+
+/**
+ * Exporta todos los tipos de dato a CSV
+ */
+public class CsvExporter {
+}
---
+++ src/main/java/org/spotify_cli/storage/JsonExporter.java
@@ -0,0 +1,7 @@
+package org.spotify_cli.storage;
+
+/**
+ * Exporta todos los tipos de datos a JSON
+ */
+public class JsonExporter {
+}
