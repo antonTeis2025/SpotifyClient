a47211e8252e2d882068fd2fbc9feb98987ae57e
Antón Davila <anton.davila.carro@gmail.com>
2025-11-06 19:10:36 +0100

    gitignore para que no se hagan publicas mis credenciales

--- src/main/java/org/spotify_cli/Main.java
+++ src/main/java/org/spotify_cli/Main.java
@@ -1,10 +1,14 @@
 package org.spotify_cli;
 
+import org.spotify_cli.api.ApiClient;
 import org.spotify_cli.database.DatabaseService;
 
-public class Main {
-    public static void main(String[] args) {
-        DatabaseService db = new DatabaseService();
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // DatabaseService db = new DatabaseService();
+        ApiClient api = new ApiClient();
+        System.out.println(api.getTokenAPI());
     }
 }
